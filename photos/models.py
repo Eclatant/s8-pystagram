7c411136839b65b9ae5a91fa19c962fd93eeb4fb
Kay Cha <kay@hannal.net>
2016-11-05 17:45:53 +0900

    Add Tag model

--- photos/models.py
+++ photos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     content = models.TextField()
+    tags = models.ManyToManyField('Tag')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -14,4 +15,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
 
