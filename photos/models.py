79345dd569bdf274d2501b0bccfe10da79c86a17
Kay Cha <kay@hannal.net>
2016-11-05 16:47:53 +0900

    Add __str__()

--- photos/models.py
+++ photos/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return '{}'.format(self.pk)
+
