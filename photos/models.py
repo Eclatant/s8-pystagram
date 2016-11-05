d17dfb49c2142f8061dcb971c3f6ccf2aafaf08a
Kay Cha <kay@hannal.net>
2016-11-05 16:51:28 +0900

    Add Comment model

--- photos/models.py
+++ photos/models.py
@@ -9,3 +9,9 @@ class Post(models.Model):
     def __str__(self):
         return '{}'.format(self.pk)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
