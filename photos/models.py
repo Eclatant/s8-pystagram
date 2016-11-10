f747f351ffb30224000e370821c7be4281e221c1
Kay Cha <kay@hannal.net>
2016-11-10 20:19:56 +0900

    Add Comment, Tag models

--- photos/models.py
+++ photos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
+    parent = models.ForeignKey('self', null=True)
 
 
 class Post(models.Model):
