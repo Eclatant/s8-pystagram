4af3131bb124e950746ffc53386d29ac29d1050f
Kay Cha <kay@hannal.net>
2016-11-10 20:41:07 +0900

    Fix Post data ordering

--- photos/views.py
+++ photos/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 
 def list_posts(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_at')
     ctx = {
         'posts': posts,
     }
