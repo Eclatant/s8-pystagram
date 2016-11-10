345faf1a8362645a8bb30ef321184eeb9efc2ed9
Kay Cha <kay@hannal.net>
2016-11-10 20:32:53 +0900

    Add posts from Post model

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+#from photos.models import Post
+from .models import Post
+
 
 def list_posts(request):
-    ctx = {}
+    posts = Post.objects.all()
+    ctx = {
+        'posts': posts,
+    }
     return render(request, 'list.html', ctx)
 
 
