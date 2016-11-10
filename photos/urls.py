4222bb0eed1334c2c37918489bfa8c2116c4b1fd
Kay Cha <kay@hannal.net>
2016-11-10 22:40:17 +0900

    Add namespace for app urls

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'photos'
+
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.view_post, name='view'),
     url(r'^$', views.list_posts, name='list'),
