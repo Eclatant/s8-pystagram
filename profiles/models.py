9d41523419a7374fabb751485cc807ab5271c77d
Doojin Kang <doojin.kang@gmail.com>
2016-11-29 16:06:16 +0900

    Fix Profile Model type error

--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext as _
 class Profile(models.Model):
     _genders = (
         ('M', _('Male'),   ),
-        ('F', _('Femail'), ),
+        ('F', _('Female'), ),
     )
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     gender = models.CharField(max_length=1,
