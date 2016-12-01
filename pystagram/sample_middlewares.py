498ee6c727ff74fd26726fd1bc462cb98d0c0b2b
Kay Cha <kay@hannal.net>
2016-12-01 20:39:58 +0900

    Add status code

--- pystagram/sample_middlewares.py
+++ pystagram/sample_middlewares.py
@@ -14,5 +14,5 @@ class SampleMiddleware(MiddlewareMixin):
                 'error': exc,
                 'status': 500,
             }
-            return render(request, 'error.html', ctx)
+            return render(request, 'error.html', ctx, status=500)
 
