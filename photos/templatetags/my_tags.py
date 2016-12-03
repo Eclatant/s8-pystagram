9da29483599771cc7e6a0b0d230e0c753500d07b
Doojin Kang <doojin.kang@gmail.com>
2016-12-03 14:19:40 +0900

    Add Template Simple Tag

--- photos/templatetags/my_tags.py
+++ photos/templatetags/my_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 def did_like(post, user):
     return post.like_set.filter(user=user).exists()
 
+@register.simple_tag
+def helloworld(*args, **kwargs):
+    return '<p>위치인자 {}</p><p>가변인자 {}</p>'.format(args, kwargs)
+
