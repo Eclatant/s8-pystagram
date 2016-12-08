fccd7e474bdd1bbb5657ddec6cdf2c99551197d3
Doojin Kang <doojin.kang@gmail.com>
2016-12-08 17:40:00 +0900

    Add Vintage JS example

--- statics/hello.js
+++ statics/hello.js
@@ -1,7 +1,38 @@
 var say_hello = function(msg) {
 	alert(msg);
-};
+}
 
-say_hello("lorem ipsum");
+$(document).ready(function() {
 
+    $('#filter_nav button').on('click', function(e) {
+        var value = $(this).val();
+        console.log('clicked ' + value);
+
+        $('#preview').vintage({
+            mime: 'image/png'
+        }, vintagePresets[value]);
+        return false;
+    });
+
+    $('#id_image').on('change', function(e) {
+        var reader = new FileReader();
+        reader.onerror = function(e) { console.log(e); }
+        reader.onloadend = function(e) {
+            if ( (/^data\:image\/(png|jpeg);base64/i).test(e.target.result) ) {
+                // console.log(e.target.result);
+                 $('#preview').attr('src', e.target.result).show(); 
+            }
+            else { 
+                if ( e.total == 0 || e.loaded == 0 ) {
+                    alert('파일을 가져오지 못 했습니다.'); 
+                } 
+                else { 
+                    alert('허용된 사진 파일이 아닙니다.'); 
+                }
+            }
+        }
+        reader.readAsDataURL(this.files[0]); // file --> base64 format
+    });
+
+});
 
