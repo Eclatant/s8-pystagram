afebfe23443e0499563663b5301954eb82d73948
Doojin Kang <doojin.kang@gmail.com>
2016-12-10 16:08:09 +0900

    Fix remove raven log

--- pystagram/sample_middlewares.py
+++ pystagram/sample_middlewares.py
@@ -1,6 +1,6 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.shortcuts import render
-from raven.contrib.django.raven_compat.models import sentry_exception_handler
+# from raven.contrib.django.raven_compat.models import sentry_exception_handler
 
 from .sample_exceptions import HelloWorldError
 
@@ -10,9 +10,9 @@ class SampleMiddleware(MiddlewareMixin):
 
     def process_exception(self, request, exc):
         if isinstance(exc, HelloWorldError):
-            print(111)
-            sentry_exception_handler(request=request)
-            print(222)
+            # print(111)
+            # sentry_exception_handler(request=request)
+            # print(222)
             ctx = {
                 'error': exc,
                 'status': 500,
